dd253509b15170771e4baa94c4481704e5880917
Jeremy Burman <jeremy.burman@w8lesslabs.com>
2019-04-11 11:58:40 -0400

    attempt to fix folder naming...

---
+++ samples/WindowsConsoleAppFSharp/WindowsConsoleAppFSharp/Json.fs
@@ -0,0 +1,9 @@
+﻿module Json
+
+open Newtonsoft.Json
+open W8lessLabs.GraphAPI
+
+type JsonSerializer() =
+   interface IJsonSerializer with
+      member this.Serialize(value) = JsonConvert.SerializeObject(value)
+      member this.Deserialize(value) : 'T = JsonConvert.DeserializeObject<'T>(value)
