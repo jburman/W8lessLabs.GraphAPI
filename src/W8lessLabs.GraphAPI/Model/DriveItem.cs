0a787ef1218524f12f6d04cbb7f0bca7dbd81b31
Jeremy Burman <jeremy.burman@w8lesslabs.com>
2019-07-26 23:30:06 -0400

    Add equality and comparison implementation on DriveItem

--- src/W8lessLabs.GraphAPI/Model/DriveItem.cs
+++ src/W8lessLabs.GraphAPI/Model/DriveItem.cs
@@ -2,7 +2,7 @@
 
 namespace W8lessLabs.GraphAPI
 {
-    public class DriveItem
+    public class DriveItem : IEquatable<DriveItem>, IComparable<DriveItem>
     {
         public string Id { get; set; }
         public string Name { get; set; }
@@ -15,5 +15,39 @@ namespace W8lessLabs.GraphAPI
         public Folder Folder { get; set; }
         public ItemReference ParentReference { get; set; }
         public Shared Shared { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is DriveItem)) return false;
+            return Equals((DriveItem)obj);
+        }
+
+        public bool Equals(DriveItem otherItem)
+        {
+            if (otherItem is null)
+                return false;
+            else
+                return Id == otherItem.Id && Name == otherItem.Name && ETag == otherItem.ETag;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            if (Id != null)
+                hash = hash * 31 + Id.GetHashCode();
+            if (Name != null)
+                hash = hash * 31 + Name.GetHashCode();
+            if (ETag != null)
+                hash = hash * 31 + ETag.GetHashCode();
+            return hash;
+        }
+
+        public int CompareTo(DriveItem other)
+        {
+            if (other is null || other.Name is null)
+                return 1;
+            else
+                return Name.CompareTo(other.Name);
+        }
     }
 }
